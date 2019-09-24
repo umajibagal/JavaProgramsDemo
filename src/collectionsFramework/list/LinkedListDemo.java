f292158ceaaf20a16ac307509fd9ddc02c8c3467
umajibagal <umaji.bagal.ppr@gmail.com>
2019-09-24 19:01:05 +0530

    LinkedListDemo added

---
+++ src/collectionsFramework/list/LinkedListDemo.java
@@ -0,0 +1,16 @@
+package collectionsFramework.list;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		list.add(13);
+		list.add("Abc");
+		list.add('v');
+		System.out.println(list);
+		
+	}
+
+}
